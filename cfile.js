02d9a519cb53ff118b5c2d5b0cc0e571f21bff28
netcfus <38586227+netcfus@users.noreply.github.com>
2018-04-22 10:08:53 +0100

    Add files via upload

---
+++ cfile.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+simpleCart.email = 'avelinolucas.fs@gmail.com'; //please enter your email address here
+	simpleCart.checkoutTo = PayPal;
+//	simpleCart.merchantId = "";
+//	simpleCart.checkoutTo = GoogleCheckout;
+//	simpleCart.currency = AUD;
+//	simpleCart.currency = GBP;
+    simpleCart.currency = EUR;
+//	simpleCart.taxRate  = 0.08;
+//simpleCart.shippingFlatRate = 35.00;
+//simpleCart.shippingQuantityRate = 45.00;
+/*	CartItem.prototype.shipping = function(){
+		if( this.size ){
+			switch( this.size.toLowerCase() ){
+				case 'small':
+					return this.quantity * 10.00;
+				case 'medium':
+					return this.quantity * 12.00;
+				case 'large':
+					return this.quantity * 15.00;
+				case 'bull':
+					return 45.00;
+				default:
+					return this.quantity * 5.00;
+			}
+		}
+	};
+*/
